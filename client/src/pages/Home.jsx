020c0ef96a59360a370672ed1fe9a3a1e757441d
Azzam1503 <uazzam69@gmail.com>
2023-02-07 12:34:36 +0530

    Added domain links of the server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     const fetchPosts = async () =>{
       setLoading(true);
       try{
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://img-generator-git-main-azzam1503.vercel.app/api/v1/post", {
           method: 'GET',
           headers:{
             'Content-Type': 'application/json'
